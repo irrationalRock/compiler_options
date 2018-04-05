7ca32a0642ca388e2ce964d6b94d4a465d66923c
irrationalRock <irrationalrockprogramming@gmail.com>
2018-04-05 10:20:04 -0400

    other stuff

---
+++ other_way.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+readarray a < options.txt
+
+for i in "${a[@]}"
+do
+	echo $i
+
+	sed '  sed 's/CMAKE_CXX_FLAGS_DEBUG:STRING=/$i/g' CMakeCache.txt'
+
+	../configure-cmake
+	
+	make
+
+	time ./brotli
+
+	make clean
+done
