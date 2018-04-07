7ff4ac30d82f3e8e48a2b3c6cfcd9120cbdce646
mgquan@myseneca.ca <mgquan@myseneca.ca>
2018-04-07 10:17:11 -0400

    added more stuff

--- find_faster.py
+++ find_faster.py
@@ -6,50 +6,46 @@ def parseTime(time):
     min = time[min_location-1:min_location]
     dot = time[min_location+1:sec_location]
     #print "time: " + time
-    #print "min: " + str(min) 
-    #print "dot: " + str(dot) 
+    #print "min: " + str(min)
+    #print "dot: " + str(dot)
     #print min_location
     #print sec_location
     return (int(min) * 100) + float(dot)
 
-with open("data.txt") as f:
+with open("derp.txt") as f:
     data = f.read().splitlines()
 
-real = re.compile('real: [0-9m.s]+')
-user = re.compile('user: [0-9m.s]+')
+real = re.compile('real\s[0-9m.s]+')
+user = re.compile('user\s[0-9m.s]+')
 
 list_of_real = []
 list_of_user = []
 
-#print data
-
 for x in data:
     if real.match(x) != None:
         list_of_real.append(real.match(x).group())
-    
+
 for x in data:
     if user.match(x) != None:
         list_of_user.append(user.match(x).group())
 
 max = 0
 
+#rint len(list_of_real)
+
 for x in list_of_real:
     if parseTime(x) > max:
         max = parseTime(x)
 
-print max
+print "real max:" + str(max)
 
 min = list_of_real[0]
 
 for x in list_of_real:
-    print x
     if parseTime(x) < min:
         min = parseTime(x)
 
-print min
-
-
-
+print  "real min:" + str(min)
 
 ###############################################################
 
@@ -57,14 +53,13 @@ max = 0
 min = list_of_real[0]
 
 for x in list_of_user:
-    #print parseTime(x)
     if parseTime(x) > max:
         max = parseTime(x)
 
-print max
+print "user max:" + str(max)
 
 for x in list_of_user:
     if parseTime(x) < min:
         min = parseTime(x)
 
-print min
+print "user min:" + str(min)
